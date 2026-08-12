a1d829efcd858c86538f917284f80f00ad5eafe9
vaishnavsathe <vaishnavisathe1999@gmail.com>
2026-08-12 07:46:04 +0530

    Changes made as advised

--- src/main/java/com/qa/OpenCart/pages/AddCart.java
+++ src/main/java/com/qa/OpenCart/pages/AddCart.java
@@ -1,10 +1,12 @@
 package com.qa.OpenCart.pages;
 
+import org.openqa.selenium.By;
+
 public class AddCart 
 {
-	int i=10;
+	private By cart = By.id("cart");
 	public void addcart()
 	{
-		System.out.println("Adding to the cart method");
+		System.out.println("Adding to the cart method"+cart);
 	}
 }
