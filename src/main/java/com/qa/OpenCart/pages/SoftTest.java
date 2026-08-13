a4b4c22acc91faf6c2293ad55354ac9b3c352fd3
vaishnavsathe <vaishnavisathe1999@gmail.com>
2026-08-13 09:34:46 +0530

    Added SoftTest functions

---
+++ src/main/java/com/qa/OpenCart/pages/SoftTest.java
@@ -0,0 +1,16 @@
+package com.qa.OpenCart.pages;
+
+public class SoftTest {
+
+    public void login() {
+        System.out.println("SoftTest - Login");
+    }
+
+    public void searchProduct() {
+        System.out.println("SoftTest - Search Product");
+    }
+
+    public void logout() {
+        System.out.println("SoftTest - Logout");
+    }
+}
